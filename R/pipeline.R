0112add373bb1ee37f43f874c642f32756d56538
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-07-02 16:33:46 +0100

    deal with trimming leading zeros

--- R/pipeline.R
+++ R/pipeline.R
@@ -19,7 +19,7 @@ slides <- seq(0, dt[, .N - (train_window + test_window)], by = test_window)
 
 incubation_period <- LogNormal(mean = 5, sd = 1, max = 14)
 # generation_time <- LogNormal(mean = 5.2, sd = 1.72, max = 10) # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7201952/
-generation_time <- Gamma(mean = 7.12, sd = 1.72, max=10) # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9837419/
+generation_time <- Gamma(mean = 7.12, sd = 1.72, max = 10) # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9837419/
 reporting_delay <- LogNormal(mean = 2, sd = 1, max = 10)
 delay <- incubation_period + reporting_delay
 rt_prior <- list(mean = 3, sd = 0.1)
@@ -37,12 +37,17 @@ obs <- obs_opts(
 )
 
 # TODO: remove once done testing
-so <- stan_opts(samples = 100)
-slides <- slides[1:5]
+so <- stan_opts(
+#  samples = 100
+)
+# slides <- slides[1:5]
+
+
+trim_leading_zero <- function (dt) { dt[which.min(confirm != 0):.N] }
 
 res_dt <- lapply(slides, \(slide) {
   epinow(
-    data = dt[(1:train_window)+slide],
+    data = dt[(1:train_window)+slide] |> trim_leading_zero(),
     generation_time = generation_time_opts(generation_time),
     delays = delay_opts(delay), rt = rt_opts(prior = rt_prior),
     horizon = test_window, obs = obs, logs = NULL, stan = so
