f544c604c5fa4cdaf419a56586c55edfb997668a
IzicTemi <iomolayo2541@gmail.com>
2024-07-07 17:04:52 +0200

    Add commented soln to cumulative cases

--- R/import.R
+++ R/import.R
@@ -11,6 +11,25 @@ library(data.table)
 raw_dt <- fread(.args[1], drop = c("YYYYMMDD", "total", "source"))
 raw_dt[, date := as.IDate(date, "%d-%m-%Y")]
 
+# correct_cumulative_cases <- function(cumulative_cases) {
+#   corrected_cases <- cumulative_cases
+#   n <- length(corrected_cases)
+#   for (i in 2:(n-1)) {
+#     if ((corrected_cases[i - 1] > corrected_cases[i + 1]) & (corrected_cases[i] > corrected_cases[i - 1])) {
+#       corrected_cases[i + 1] <- corrected_cases[i]
+#     } else if ((corrected_cases[i] < corrected_cases[i - 1]) & (corrected_cases[i] < corrected_cases[i + 1])) {
+#       corrected_cases[i] <- corrected_cases[i - 1]
+#     } else if ((corrected_cases[i] > corrected_cases[i - 1]) & (corrected_cases[i] > corrected_cases[i + 1])) {
+#       corrected_cases[i] <- corrected_cases[i - 1]
+#     }
+#   }
+#   # Handle the last element separately if needed
+#   if (n > 1 && corrected_cases[n] < corrected_cases[n - 1]) {
+#     corrected_cases[n] <- corrected_cases[n - 1]
+#   }
+#   return(corrected_cases)
+# }
+
 long_dt <- raw_dt |> melt.data.table(id.vars = "date", variable.name = "province", value.name = "cumulative_incidence")
 
 long_dt[
