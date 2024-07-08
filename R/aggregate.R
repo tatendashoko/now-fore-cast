a09df6595f9180ff8897ea6ba14f6ae6afba511e
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-07-08 14:20:33 +0100

    incorporate data cleaning

---
+++ R/aggregate.R
@@ -0,0 +1,13 @@
+
+library(data.table)
+
+.args <- if (interactive()) c(
+	list.files("data", "daily", full.names = TRUE) |> grep("RSA", x = _, value = TRUE, invert = TRUE),
+	file.path("data", "daily_RSA.rds")
+) else commandArgs(trailingOnly = TRUE)
+
+all_dt <- (head(.args, -1) |> lapply(readRDS) |> rbindlist())
+
+if (all_dt[,any(is.na(confirm)) & !all(is.na(confirm)), by=date][,any(V1)]) stop("mixed NAs by province.")
+
+all_dt[,.(province = "RSA", confirm = sum(confirm)), keyby=date] |> saveRDS(tail(.args, 1))
