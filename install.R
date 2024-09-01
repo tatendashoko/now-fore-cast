95ff769761648070506d5a5495aa3315f10d2c2b
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2024-08-31 22:00:02 -0400

    incorporate gdrive

--- install.R
+++ install.R
@@ -5,19 +5,3 @@ renv::activate()
 res <- renv::restore(prompt = FALSE)
 
 print(res)
-
-# if (!require(data.table)) {
-# 	install.packages("data.table")
-# }
-# 
-# if (!require(ggplot2)) {
-# 	install.packages("ggplot2")
-# }
-# 
-# if (!require(EpiNow2)) {
-# 	install.packages("EpiNow2", repos = c("https://epiforecasts.r-universe.dev", getOption("repos")))
-# }
-# 
-# if (!require(scoringutils)) {
-# 	install.packages("scoringutils")
-# }
