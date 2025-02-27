1a0bb5db7ca741a49d2e679ca3ebfc648207d31d
jamesmbaazam <james.azam@lshtm.ac.uk>
2025-02-27 23:00:27 +0000

    Remove ess per sec calculation

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -30,12 +30,5 @@ diagnostics_dt_combined <- rbind(
     fill = TRUE
 )
 
-# Calculate ESS per second
-diagnostics_dt_combined[, `:=`(
-    ess_basic_ps = ess_basic/stan_elapsed_time,
-    ess_bulk_ps = ess_bulk/stan_elapsed_time,
-    ess_tail_ps = ess_tail/stan_elapsed_time)
-]
-
 # Save as csv
 write.csv(diagnostics_dt_combined, tail(.args, 1), row.names = FALSE)
