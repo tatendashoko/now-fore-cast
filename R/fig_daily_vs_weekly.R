53074fb7297849325159e98b12cd7fe4aa9b53d0
jamesmbaazam <james.azam@lshtm.ac.uk>
2025-02-27 16:36:31 +0000

    Rotate x-axis tick labels

--- R/fig_daily_vs_weekly.R
+++ R/fig_daily_vs_weekly.R
@@ -12,21 +12,23 @@ daily_dt <- readRDS(.args[1])
 weekly_dt <- readRDS(.args[2])
 
 p <- ggplot() +
-  geom_point(
-    aes(x = date, y = confirm),
-    data = daily_dt, size = 0.5
-  ) +
-  geom_segment(
-    aes(x = date - 6.5, xend = date + 0.5, y = confirm/7, yend = confirm/7),
-    data = weekly_dt[!is.na(confirm)],
-    color = "firebrick"
-  ) +
-  scale_y_continuous(
-    "Daily Incidence", transform = "log2", sec.axis = sec_axis(
-      ~ . * 7, name = "Weekly Incidence" 
-    )
-  ) +
-  scale_x_date(NULL, date_breaks = "month", date_labels = "%b '%y") +
-  theme_minimal()
+    geom_point(
+        aes(x = date, y = confirm),
+        data = daily_dt, size = 0.5
+    ) +
+    geom_segment(
+        aes(x = date - 6.5, xend = date + 0.5, y = confirm/7, yend = confirm/7),
+        data = weekly_dt[!is.na(confirm)],
+        color = "firebrick"
+    ) +
+    scale_y_continuous(
+        "Daily Incidence", sec.axis = sec_axis(
+            ~ . * 7, name = "Weekly Incidence"
+        )
+    ) +
+    scale_x_date(NULL, date_breaks = "month", date_labels = "%b '%y") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+    # theme_minimal()
+    NULL
 
 ggsave(tail(.args, 1), p, bg = "white", width = 12, height = 6)
